4d71573223041f1f2242047c64ed5ae21d28d96a
githmith <vuminhthanh2305@gmail.com>
2019-11-08 21:14:59 -0500

    Fix remove 'Toggle' text of menu toggler

--- assets/js/main.js
+++ assets/js/main.js
@@ -104,7 +104,8 @@
 			// Toggle.
 				if (skel.vars.IEVersion > 9) {
 
-					$('<a href="#sidebar" class="toggle">Toggle</a>')
+					// $('<a href="#sidebar" class="toggle">Toggle</a>')
+					$('<a href="#sidebar" class="toggle"></a>') /*thanh*/
 						.appendTo($sidebar)
 						.on('click', function(event) {
 
